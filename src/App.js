7bb60d1000c61f764275b95abed707b660a747d3
macfrei <lene@7freiheiten.de>
2021-02-23 10:42:38 +0100

    Add exercise with props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 /*
- * Let's add a button component.
+ * I added a state 'isVisible' and an onClick
+ * to our button.
  *
- * A simple button version which we can
- * converte to another button version in
- * our app
+ * The subtitle should not be visible after a click.
+ * For this you need to pass the prop to your header
+ * component.
+ *
+ * The button should change on hover.
  *
  * Don't forget to import 'styled-components/macro'!
  */
 
-import styled from 'styled-components/macro'
+import { useState } from 'react'
 import AppHeader from './AppHeader'
 import Button from './Button'
 
 export default function App() {
+  const [isVisible, setIsVisible] = useState(true)
+
   return (
     <div>
       <AppHeader title={'Hi Hacker!'} subtitle={"How's it going?"} />
-      <Button>Button</Button>
-      <FilterButton>Filter Button</FilterButton>
+      <Button onClick={() => setIsVisible(!isVisible)}>Button</Button>
     </div>
   )
 }
-
-const FilterButton = styled(Button)`
-  background-color: black;
-  color: white;
-`
